a949772654018f90e60d1663addb0f91d9ee4ab3
vianvio <gloomy_wind@hotmail.com>
2016-06-15 11:13:10 +0800

    ready for travis-ci & git auto deploy

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require("assert")
+describe('Array', function(){
+  describe('sample test array', function(){
+    it('just for pass test', function(){
+      assert.equal(1, [1,2,3].indexOf(0));
+    })
+  })
+})
