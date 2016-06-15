99fadf8dffabacdb742679baad0502fd4868ef52
vianvio <gloomy_wind@hotmail.com>
2016-06-15 11:53:55 +0800

    fix test case

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var assert = require("assert")
 describe('Array', function(){
   describe('sample test array', function(){
     it('just for pass test', function(){
-      assert.equal(1, [1,2,3].indexOf(0));
+      assert.equal(0, [1,2,3].indexOf(1));
     })
   })
 })
